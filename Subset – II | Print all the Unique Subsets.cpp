e6a541f834864b9e7aea3e9661e961c045a63f83
Mohit Hooda <mhooda87@gmail.com>
2022-05-03 13:01:32 +0530

    Create Subset – II | Print all the Unique Subsets.cpp

---
+++ Subset – II | Print all the Unique Subsets.cpp
@@ -0,0 +1,39 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void generateSubsets(int ind,vector<int> &v,vector<int> &ds, vector<vector<int>> &ans){
+    ans.push_back(ds);
+    for(int i=ind;i<v.size();i++){
+        if(i!=ind && v[i-1]==v[i]){
+            continue;
+        }
+        ds.push_back(v[i]);
+        generateSubsets(i+1,v,ds,ans);
+        ds.pop_back();
+    }
+}
+
+int main() {
+    int n;
+    cin>>n;
+    vector<int> v;
+    vector<int> ds;
+    for(int i=0;i<n;i++){
+        int a;
+        cin>>a;
+        v.push_back(a);
+    }
+    sort(v.begin(),v.end());
+    vector<vector<int>> ans;
+    generateSubsets(0,v,ds,ans);
+    for(int i=0;i<ans.size();i++){
+        for(int j=0;j<ans[i].size();j++){
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}

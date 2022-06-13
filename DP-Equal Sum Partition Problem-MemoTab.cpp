aac86d8d2eee37e1b5328ef9d3a68648373d15f2
Mohit Hooda <mhooda87@gmail.com>
2022-06-13 23:23:35 +0530

    Create DP-Equal Sum Partition Problem-MemoTab.cpp

---
+++ DP-Equal Sum Partition Problem-MemoTab.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int f(int ind,vector<int> &a,int target,vector<vector<int>>dp){
+    if(target==0){
+        return 1;
+    }
+    if(ind==0){
+        return 0;
+    }
+    if(dp[ind][target]!=0){
+        return dp[ind][target];
+    }
+    
+    return dp[ind][target] = (f(ind-1,a,target - a[ind],dp)) || (f(ind-1,a,target,dp));
+    return dp[ind][target] =  f(ind-1,a,target,dp);
+    
+    
+}
+int main() {
+    int n,target;
+    cin>>n;
+    vector<int> a;
+    
+    int totalSum = 0;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        a.push_back(x);
+        totalSum +=x;
+    }
+    if(totalSum%2!=0){
+        cout<<"Not possible";
+        exit(0);
+    }
+    
+    target = totalSum/2;
+    vector<vector<int>> dp(n+1 , vector<int> (target+1,0));
+    for(int i=0;i<=n;i++){
+        dp[i][0] = 1;
+    }
+    for(int i=0;i<=target;i++){
+        dp[0][i] = 0;
+    }
+    dp[0][0] = 1;
+    cout<<"Memoization "<<f(n-1,a,target,dp)<<endl;
+    vector<vector<int>> dp_tab(n+1 , vector<int> (target+1,0));
+    for(int i=0;i<=n;i++){
+        dp_tab[i][0] = 1;
+    }
+    for(int i=0;i<=target;i++){
+        dp_tab[0][i] = 0;
+    }
+    dp_tab[0][0] = 1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=target;j++){
+            if(a[i-1] <= j){
+                dp_tab[i][j] = int( dp_tab[i-1][j-a[i-1]] || dp_tab[i-1][j]);
+            }else{
+                dp_tab[i][j] = dp_tab[i-1][j];
+            }
+        }
+    }
+    cout<<"Tabular "<<dp_tab[n][target]<<endl;
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<target+1;j++){
+            cout<<dp[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<target+1;j++){
+            cout<<dp_tab[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+	return 0;
+}

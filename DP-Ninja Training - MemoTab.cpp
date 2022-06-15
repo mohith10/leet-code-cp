4257d91674792f5bed842614fc64fa421ba740d6
Mohit Hooda <mhooda87@gmail.com>
2022-06-15 13:11:41 +0530

    Update DP-Ninja Training - MemoTab.cpp

--- DP-Ninja Training - MemoTab.cpp
+++ DP-Ninja Training - MemoTab.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int f(int day,int lastTask, vector<vector<int>> &a, vector<vector<int>> dp){
+int f(int day,int lastTask, vector<vector<int>> &a, vector<vector<int>> &dp){
     if(day == 0){
         int maxi = 0;
         for(int i=0;i<3;i++){
@@ -37,22 +37,29 @@ int main() {
 	    a[i][2] = z;
 	}
 	cout<<"Memoization "<<f(n-1,3,a,dp)<<endl;
-	vector<vector<int>> dp_tab(n,vector<int> (4,-1));
-	dp_tab[0][0] = max(a[0][1],a[0][2]);
-	dp_tab[0][1] = max(a[0][0],a[0][2]);
-	dp_tab[0][2] = max(a[0][1],a[0][0]);
-	dp_tab[0][3] = max(a[0][0],max(a[0][1],a[0][2]));
+
+	vector<vector<int>> dp_tab(n,vector<int> (4,0));
+	int maxi = max(a[0][1],a[0][2]);
+    dp_tab[0][0] = maxi;
+    maxi = max(a[0][0],a[0][2]);
+    dp_tab[0][1] = maxi;
+    maxi = max(a[0][0],a[0][1]);
+    dp_tab[0][2] = maxi;
+    maxi = max(a[0][0],max(a[0][1],a[0][2]));
+    dp_tab[0][3] = maxi;
 	for(int i=1;i<n;i++){
-	    for(int j=0;j<3;j++){
-	        if(j==0){
-	            dp_tab[i][j]=max(dp_tab[i][1],dp_tab[i][2]);
-	        }else if(j==1){
-	            dp_tab[i][j]=max(dp_tab[i][0],dp_tab[i][2]);
-	        }else{
-	            dp_tab[i][j]=max(dp_tab[i][0],dp_tab[i][1]);
-	        }
+	    for(int lastTask=0;lastTask<4;lastTask++){
+	        dp_tab[i][lastTask] = 0;
+	        int maxiRes=0;
+	        for(int task=0;task<3;task++){
+                if(lastTask!=task){
+                    maxiRes =  max(a[i][task] + dp_tab[i - 1][task],maxiRes);
+                }
+            }
+            dp_tab[i][lastTask] = maxiRes;
 	    }
 	}
-	cout<<max(dp_tab[n-1][0],dp_tab[n-1][1],dp_tab[n-1][2]);
+	//maxi = max(dp_tab[n-1][0],dp_tab[n-1][1],dp_tab[n-1][2]);
+	cout<<"Tabular "<<dp_tab[n-1][3];
 	return 0;
 }

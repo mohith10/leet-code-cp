64c70b5499b7d3e77c4a1e85f8db7a05377bbaf9
Mohit Hooda <mhooda87@gmail.com>
2022-08-01 01:25:20 +0530

    Create Linked List - Template of Linked List.cpp

---
+++ Linked List - Template of Linked List.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+};
+
+Node* insert(Node* head, int value){
+    if(head == NULL){
+       Node* newNode = new Node();
+       newNode->data = value;
+       newNode->next = NULL;
+       return newNode;
+    }
+    Node* listIterator = head;
+    while(listIterator->next!=NULL){
+        listIterator = listIterator->next;
+    }
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->next = NULL;
+    listIterator->next = newNode;
+    return head;
+}
+
+void printLinkedlist(Node* head){
+    Node* listIterator = head;
+    while(listIterator!=NULL){
+        cout<<listIterator->data<<" ";
+        listIterator = listIterator->next;
+    }
+}
+int main() {
+	Node* head = NULL;
+	head = insert(head, 3);
+	insert(head,4);
+	insert(head,5);
+	printLinkedlist(head);
+	return 0;
+}

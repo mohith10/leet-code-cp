e5c849a78ae4e2faf2acd4517886cdd293ffb195
Mohit Hooda <mhooda87@gmail.com>
2022-08-10 02:17:07 +0530

    Update BS-Array Rotation.cpp

--- BS-Array Rotation.cpp
+++ BS-Array Rotation.cpp
@@ -1,31 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int find_index(vector<int> &a, int n){
-    int start = 0, end = n-1;
-    while(start<=end){
-        cout<<start<<" "<<end<<endl;
-        int mid = start + (end - start)/2;
-        int next = (mid+1)%n;
-        int prev = (mid-1+n)%n;
-        if(a[mid]<=a[prev] && a[mid]<=a[next]) return mid;
-        else if(a[mid]<=a[end]) end = mid-1;
-        else start = mid+1;
+int findOriginOfRotation(vector<int> arr, int num){
+    int low = 0, high = num-1;
+    while(low<high){
+        int mid = low + (high-low)/2;
+        int prev = (mid-1+num)%num;
+        int next = (mid+1)%num;
+        if(arr[mid] < arr[prev] && arr[mid] < arr[next]) return mid;
+        if(arr[mid] < arr[high]){
+            high = mid-1;
+        }else{
+            low = mid + 1;
+        }
     }
-    return 0;
+    
 }
+
 int main() {
-	// your code goes here
-	int n;
-	cin>>n;
-	vector<int> arr;
-	for(int i=0;i<n;i++){
-	    int x;
-	    cin>>x;
-	    arr.push_back(x);
-	}
-	
-	int ind = find_index(arr,n);
-	cout<<"Array rotated by "<<n-ind;
+    int num;
+    cin>>num;
+    vector<int> arr;
+    for(int ind = 0; ind<num; ind++){
+        int x;
+        cin>>x;
+        arr.push_back(x);
+    }
+    int origin = findOriginOfRotation(arr,num);
+    cout<<origin;
 	return 0;
 }
